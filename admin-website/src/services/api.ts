4c7a4204e6cd12786cde33bf131406a36f74a3cc
Matheus Alves <theuves@gmail.com>
2020-11-20 16:07:16 -0400

    Add option to load BASE_URL from environment

--- admin-website/src/services/api.ts
+++ admin-website/src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL:
+    process.env.NODE_ENV === 'production'
+      ? process.env.REACT_APP_BASE_URL
+      : 'http://localhost:3333',
 });
 
 export default api;
